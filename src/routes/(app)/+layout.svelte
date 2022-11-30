7415d08c7d37f05c0a8629c86b5978908245bdf1
LarsG <lars.ga.solheim@gmail.com>
2022-12-01 00:25:11 +0100

    moved background image into tailwind config

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -2,15 +2,10 @@
 	import Navbar from '$lib/Navbar.svelte';
 	import Footer from '$lib/Footer.svelte';
 	import '../../app.css';
-
-	const backgroundImageURL =
-		'https://images.unsplash.com/photo-1446776811953-b23d57bd21aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80';
 </script>
 
 <div class="h-screen relative flex flex-col">
-	<div
-		class="h-screen w-full bg-no-repeat bg-center bg-cover fixed -z-10 bg-[url('{backgroundImageURL}')]"
-	/>
+	<div class="h-screen w-full bg-no-repeat bg-center bg-cover fixed -z-10 bg-site-background" />
 
 	<Navbar />
 	<main class="flex-grow flex flex-col items-center mt-20">
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			backgroundImage: {
+        'site-background':
+          "linear-gradient(90deg, rgba(2,0,36,0.5) 0%, rgba(9,9,121,0.1) 20%, rgba(9,9,121,0.1) 80%, rgba(2,0,36,0.5) 100%), url('https://images.unsplash.com/photo-1446776811953-b23d57bd21aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80')",
+      },
+		}
 	},
 	plugins: []
 };
