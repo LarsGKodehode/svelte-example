cf7b07f9c8dbec0899d038a1cbe4688de9e38f02
LarsG <lars.ga.solheim@gmail.com>
2022-11-30 15:44:53 +0100

    basic setup of example application

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,8 @@
+<footer class="w-full bg-purple-600 px-6 pb-4 pt-2">
+  <nav>
+    <ul>
+      <li><a target="_blank" rel="noreferrer" href="https://github.com/LarsGKodehode">GitHub</a></li>
+      <li><a target="_blank" rel="noreferrer" href="www.linkedin.com/in/lars-gunnar-solheim-99818b249">LinkedIn</a></li>
+    </ul>
+  </nav>
+</footer>
---
+++ src/routes/(app)/about/+page.svelte
@@ -0,0 +1,4 @@
+<div class="px-4 py-2">
+  <h1>Landing page</h1>
+  
+</div>
